d7877b3c32c4531381cb7f64768a936eb91e01bd
Ik3nna <iamiykendukwu@gmail.com>
2022-10-23 14:46:36 +0100

    Profile section

--- Task1/src/Home.js
+++ Task1/src/Home.js
@@ -3,7 +3,13 @@ import React from "react";
 function Home() {
     return(
         <main>
-            
+            {/* Profile Section */}
+            <img src="/assets/profile__img.png" alt="profile_img" />
+
+            <p>_Ik3nna</p>
+            <p>ikenna</p>
+
+            {/* Links section */}
         </main>
     );
 }
