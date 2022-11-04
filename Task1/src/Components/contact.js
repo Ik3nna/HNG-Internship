96b627a3884ca340c405a5938a702dac282fa422
Ik3nna <iamiykendukwu@gmail.com>
2022-11-04 04:09:43 +0100

    Fix: Correction to the setup of Bootstrap

--- Task1/src/Components/contact.js
+++ Task1/src/Components/contact.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
 function Contact () {
-
+    
 }
 
 export default Contact;
