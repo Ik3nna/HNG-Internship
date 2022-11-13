216e6223cef74b47bca5c577f53a94c5bdc35062
Ik3nna <iamiykendukwu@gmail.com>
2022-11-13 15:57:18 +0100

    Styles: Styling of the header

--- Task 3/src/components/header.js
+++ Task 3/src/components/header.js
@@ -1,48 +1,51 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function Header() {
     return(
-        <Container>
-            <header>
-                <img src="/assets/logo.svg" alt="logo" />
+        <header>
+           <img src="/assets/logo.svg" alt="logo" />
 
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/home" className={({isActive})=> isActive ? "activeLink" : null}>
-                                Home
-                            </NavLink>
-                        </li>
+            <nav>
+                <ul className="d-flex align-items-center mt-4">
+                    <li>
+                        <NavLink to="/home" className={({isActive})=> isActive ? "activeLink" : null}>
+                            Home
+                        </NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/place" className={({isActive})=> isActive ? "activeLink" : null}>
-                                Place to stay
-                            </NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/place" className={({isActive})=> isActive ? "activeLink" : null}>
+                            Place to stay
+                        </NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/nfts" className={({isActive})=> isActive ? "activeLink" : null}>
-                                NFTs
-                            </NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/nfts" className={({isActive})=> isActive ? "activeLink" : null}>
+                            NFTs
+                        </NavLink>
+                    </li>
 
-                        <li>
-                            <NavLink to="/community" className={({isActive})=> isActive ? "activeLink" : null}>
-                                Community
-                            </NavLink>
-                        </li>
+                    <li>
+                        <NavLink to="/community" className={({isActive})=> isActive ? "activeLink" : null}>
+                            Community
+                        </NavLink>
+                    </li>
 
-                        <li>
-                            <button>
-                                Connect wallet
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </Container>
+                    <li>
+                        <button>
+                            Connect wallet
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="overlay"></div>
+            
+            <div className="hamburger-menu">
+                <div className="bar"></div>
+            </div>
+        </header>
     );
 }
 
