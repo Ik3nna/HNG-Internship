8e490aade46218f3f9833ed9a942c9cd4f1802a8
Ik3nna <iamiykendukwu@gmail.com>
2022-11-13 18:36:18 +0100

    Style: Addition of fixed navbar

--- Task 3/src/components/header.js
+++ Task 3/src/components/header.js
@@ -1,51 +1,64 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import useSticky from "./utils";
 
 function Header() {
+    const { sticky, stickyRef } = useSticky();
+
     return(
-        <header>
-           <img src="/assets/logo.svg" alt="logo" />
+        <>
+            <header ref={stickyRef} className={`${sticky && "fixed-nav"}`}>
+                <div className="head">
+                    <Link to="/" className="logo">
+                        <img src="/assets/logo.svg" alt="logo" />
+                    </Link>
 
-            <nav>
-                <ul className="d-flex align-items-center mt-4">
-                    <li>
-                        <NavLink to="/home" className={({isActive})=> isActive ? "activeLink" : null}>
-                            Home
-                        </NavLink>
-                    </li>
+                    <nav>
+                        <ul className="d-flex align-items-center mt-4">
+                            <li>
+                                <NavLink to="/home" className={({isActive})=> isActive ? "activeLink" : null}>
+                                    Home
+                                </NavLink>
+                            </li>
 
-                    <li>
-                        <NavLink to="/place" className={({isActive})=> isActive ? "activeLink" : null}>
-                            Place to stay
-                        </NavLink>
-                    </li>
+                            <li>
+                                <NavLink to="/place" className={({isActive})=> isActive ? "activeLink" : null}>
+                                    Place to stay
+                                </NavLink>
+                            </li>
 
-                    <li>
-                        <NavLink to="/nfts" className={({isActive})=> isActive ? "activeLink" : null}>
-                            NFTs
-                        </NavLink>
-                    </li>
+                            <li>
+                                <NavLink to="/nfts" className={({isActive})=> isActive ? "activeLink" : null}>
+                                    NFTs
+                                </NavLink>
+                            </li>
 
-                    <li>
-                        <NavLink to="/community" className={({isActive})=> isActive ? "activeLink" : null}>
-                            Community
-                        </NavLink>
-                    </li>
+                            <li>
+                                <NavLink to="/community" className={({isActive})=> isActive ? "activeLink" : null}>
+                                    Community
+                                </NavLink>
+                            </li>
 
-                    <li>
-                        <button>
-                            Connect wallet
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+                            <li>
+                                <button>
+                                    Connect wallet
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
 
-            <div className="overlay"></div>
-            
-            <div className="hamburger-menu">
-                <div className="bar"></div>
-            </div>
-        </header>
+                    <div className="overlay"></div>
+                    
+                    <div className="hamburger-menu">
+                        <div className="bar"></div>
+                    </div>
+                </div>
+            </header>
+
+            <div style={{
+            height: sticky ? `${stickyRef.current?.clientHeight}px` : '0px',
+            }} />
+        </>
     );
 }
 
