52897c443289797fd04026a4c991c42a72773f19
Ik3nna <iamiykendukwu@gmail.com>
2022-11-14 03:43:58 +0100

    Styles: Responsiveness of Metabnb component

--- Task 3/src/components/NFTs-Section/metabnb.js
+++ Task 3/src/components/NFTs-Section/metabnb.js
@@ -8,7 +8,7 @@ function Metabnb () {
     return(
         <section className={`mt-3 ${styles.content}`}>
             <Row className={styles.row}>
-                <Col md={6}>
+                <Col xs={{ span: 12, order: "last" }} lg={{ span: 6, order: "first"}}>
                     <h2>
                         Metabnb NFTs
                     </h2>
@@ -22,6 +22,10 @@ function Metabnb () {
                     <Link to="/nfts">
                         Learn more
                     </Link>
+                </Col>
+
+                <Col xs={{ span: 12, order: "first" }} lg={{ span: 6, order: "last"}} className={styles.imgContainer}>
+                    <img src="/assets/Frame 59546.svg" alt="frame-59546" />
                 </Col>
             </Row>
             
